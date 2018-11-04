c2081a2655f2d7bd1f627d6c0d4706924f33307d
Jonathan Phoon <jonathan.pwh@gmail.com>
2018-11-04 18:35:26 +0800

    ADD setup script for mac os

---
+++ setup.sh
@@ -0,0 +1,130 @@
+# Colors
+ESC_SEQ="\x1b["
+COL_RESET=$ESC_SEQ"39;49;00m"
+COL_RED=$ESC_SEQ"31;01m"
+COL_GREEN=$ESC_SEQ"32;01m"
+COL_YELLOW=$ESC_SEQ"33;01m"
+COL_BLUE=$ESC_SEQ"34;01m"
+COL_MAGENTA=$ESC_SEQ"35;01m"
+COL_CYAN=$ESC_SEQ"36;01m"
+
+# Helpers
+function ok() {
+    echo -e "$COL_GREEN[ok]$COL_RESET "$1
+}
+
+function bot() {
+    echo -e "\n$COL_GREEN\[._.]/$COL_RESET - "$1
+}
+
+function running() {
+    echo -en "$COL_YELLOW ⇒ $COL_RESET"$1": "
+}
+
+function action() {
+    echo -e "\n$COL_YELLOW[action]:$COL_RESET\n ⇒ $1..."
+}
+
+function warn() {
+    echo -e "$COL_YELLOW[warning]$COL_RESET "$1
+}
+
+function error() {
+    echo -e "$COL_RED[error]$COL_RESET "$1
+}
+
+function reboot() {
+  read -p "Do you want to reboot your computer now? (y/N)" choice
+  case "$choice" in
+    y | Yes | yes ) echo "Yes"; exit;; # If y | yes, reboot
+    n | N | No | no) echo "No"; exit;; # If n | no, exit
+    * ) echo "Invalid answer. Enter \"y/yes\" or \"N/no\"" && return;;
+  esac
+}
+
+# Requirers
+function require_cask() {
+    running "brew cask $1"
+    brew cask list $1 > /dev/null 2>&1 | true
+    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+        action "brew cask install $1 $2"
+        brew cask install $1
+        if [[ $? != 0 ]]; then
+            error "failed to install $1! aborting..."
+            # exit -1
+        fi
+    fi
+    ok
+}
+
+function require_brew() {
+    running "brew $1 $2"
+    brew list $1 > /dev/null 2>&1 | true
+    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+        action "brew install $1 $2"
+        brew install $1 $2
+        if [[ $? != 0 ]]; then
+            error "failed to install $1! aborting..."
+            # exit -1
+        fi
+    fi
+    ok
+}
+
+###############################################################################
+# Install Homebrew (CLI Packages)                                             #
+###############################################################################
+
+running "checking homebrew install"
+brew_bin=$(which brew) 2>&1 > /dev/null
+if [[ $? != 0 ]]; then
+  action "installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    if [[ $? != 0 ]]; then
+      error "unable to install homebrew, script $0 abort!"
+      exit 2
+  fi
+else
+  ok
+  # Make sure we’re using the latest Homebrew
+  running "updating homebrew"
+  brew update
+  ok
+  bot "before installing brew packages, we will upgrade any outdated packages."
+      # Upgrade any already-installed formulae
+      action "upgrade brew packages..."
+      brew upgrade
+      ok "brews updated..."
+fi
+
+###############################################################################
+# Install Homebrew Cask (UI Packages)                                         #
+###############################################################################
+
+running "checking brew-cask install"
+output=$(brew tap | grep cask)
+if [[ $? != 0 ]]; then
+  action "installing brew-cask"
+  require_brew caskroom/cask/brew-cask
+fi
+brew tap caskroom/versions > /dev/null 2>&1
+ok
+
+action "Installing Git"
+require_brew git
+ok
+
+action "Installing Node"
+require_brew node
+ok
+
+action "Installing google cloud cli"
+require_cask google-cloud-sdk
+ok
+
+action "Installing vscode"
+require_cask visual-studio-code
+ok
+
+action "Setting up gcloud"
+gcloud init
