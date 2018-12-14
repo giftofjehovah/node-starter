49d821eddb597e3b42e3af739b69d283fc564a3a
Jonathan Phoon <jonathan.pwh@gmail.com>
2018-12-14 17:42:27 +0800

    ADD firebase and functions

---
+++ service/functions/index.js
@@ -0,0 +1,20 @@
+const functions = require('firebase-functions')
+const firebase = require('firebase-admin')
+
+const config = {
+  apiKey: 'AIzaSyAGoO96ia2-hRG20au-i3qIH23aiy8yvQ0',
+  authDomain: 'byob-chatbot.firebaseapp.com',
+  databaseURL: 'https://byob-chatbot.firebaseio.com'
+}
+firebase.initializeApp(config)
+const db = firebase.database()
+
+exports.getRoom = functions.https.onRequest((req, res) => {
+  console.log(req.body)
+  db.ref('/rooms')
+    .once('value')
+    .then(snapshot => console.log(snapshot.val()))
+  res.json({ messages: [{ text: 'Hi man' }] })
+})
+
+exports.hello = functions.https.onRequest((req, res) => res.send(`Hello World! :)`))
